6620fcb7084bd4fa7bb2a8ca9e47ec921fd80b45
SSSerpenttt <77667524+SSSerpenttt@users.noreply.github.com>
2025-05-20 16:07:46 +0800

    Update modified-maskrcnn.py

--- mmdet/configs/_base_/models/modified-maskrcnn.py
+++ mmdet/configs/_base_/models/modified-maskrcnn.py
@@ -1,7 +1,7 @@
 model = dict(
     type='MaskRCNN',
     backbone=dict(
-        type='EfficientNetD3Backbone',  # Custom backbone
+        type='EfficientNetD3Backbone',
         pretrained=True
     ),
     neck=dict(
@@ -10,23 +10,62 @@ model = dict(
         out_channels=160,
         num_outs=5
     ),
-rpn_head=dict(
-    type='RepPointsHead',
-    num_classes=80,
-    in_channels=160,
-    feat_channels=160,
-    point_feat_channels=160,
-    stacked_convs=3,
-    num_points=9,
-    gradient_mul=0.3,
-    point_strides=[8, 16, 32, 64, 128],
-    point_base_scale=4,
-    loss_cls=dict(
-        type='FocalLoss',
-        use_sigmoid=True,
-        gamma=2.0,
-        alpha=0.25,
-        loss_weight=1.0),
-    loss_bbox_init=dict(type='SmoothL1Loss', beta=1.0, loss_weight=0.5),
-    loss_bbox_refine=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0)
+    rpn_head=dict(
+        type='RepPointsHead',
+        num_classes=80,
+        in_channels=160,
+        feat_channels=160,
+        point_feat_channels=160,
+        stacked_convs=3,
+        num_points=9,
+        gradient_mul=0.3,
+        point_strides=[8, 16, 32, 64, 128],
+        point_base_scale=4,
+        transform_method='minmax',  # <--- your function uses this
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox_init=dict(type='SmoothL1Loss', beta=1.0, loss_weight=0.5),
+        loss_bbox_refine=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0)
+    ),
+    roi_head=dict(
+        type='StandardRoIHead',
+        bbox_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(
+                type='MultiScaleRoIAlign',  # <- updated
+                output_size=7,
+                sampling_ratio=2
+            ),
+            out_channels=160,
+            featmap_strides=[4, 8, 16, 32]
+        ),
+        bbox_head=dict(
+            type='Shared2FCBBoxHead',
+            in_channels=160,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=80
+        ),
+        mask_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(
+                type='MultiScaleRoIAlign',  # <- updated
+                output_size=14,
+                sampling_ratio=2
+            ),
+            out_channels=160,
+            featmap_strides=[4, 8, 16, 32]
+        ),
+        mask_head=dict(
+            type='FCNMaskHead',
+            num_convs=4,
+            in_channels=160,
+            conv_out_channels=256,
+            num_classes=80
+        )
+    )
 )
