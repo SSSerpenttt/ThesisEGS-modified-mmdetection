42287ca154687d0bfd00077f35c908bde426fc1b
SSSerpenttt <77667524+SSSerpenttt@users.noreply.github.com>
2025-05-20 15:34:57 +0800

    Update modified_maskrcnn.py
    - defined base

--- configs/_base_/models/modified_maskrcnn.py
+++ configs/_base_/models/modified_maskrcnn.py
@@ -1,3 +1,9 @@
+_base_ = [
+    'mmdet::_base_/datasets/coco_detection.py',
+    'mmdet::_base_/schedules/schedule_1x.py',
+    'mmdet::_base_/default_runtime.py'
+]
+
 model = dict(
     type='MaskRCNN',
     backbone=dict(
