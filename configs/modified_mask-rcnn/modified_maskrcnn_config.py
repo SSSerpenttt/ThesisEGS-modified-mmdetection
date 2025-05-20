c84ae8c20e21288553c05d04489b76f0067e272f
SSSerpenttt <77667524+SSSerpenttt@users.noreply.github.com>
2025-05-20 15:06:01 +0800

    Update modified_maskrcnn_config.py
    - Loaded modified model from repository

--- configs/modified_mask-rcnn/modified_maskrcnn_config.py
+++ configs/modified_mask-rcnn/modified_maskrcnn_config.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/mask-rcnn_efficientdet_d3_bifpn.py',  # Replace ResNet-101 + FPN
+    '../_base_/models/modified_maskrcnn.py', #From _models_ diretory
     '../_base_/datasets/coco_instance.py',
     '../_base_/schedules/schedule_1x.py',
     '../_base_/default_runtime.py'
