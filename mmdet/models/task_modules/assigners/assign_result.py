619e1a92234e15b018fa5ce7ec1498ecd9405246
SSSerpenttt <77667524+SSSerpenttt@users.noreply.github.com>
2025-05-24 12:26:42 +0800

    Update assign_result.py

--- mmdet/models/task_modules/assigners/assign_result.py
+++ mmdet/models/task_modules/assigners/assign_result.py
@@ -18,54 +18,16 @@ class AssignResult(util_mixins.NiceRepr):
             assigned truth box.
         labels (Tensor): If specified, for each predicted box
             indicates the category label of the assigned truth box.
-
-    Example:
-        >>> # An assign result between 4 predicted boxes and 9 true boxes
-        >>> # where only two boxes were assigned.
-        >>> num_gts = 9
-        >>> max_overlaps = torch.LongTensor([0, .5, .9, 0])
-        >>> gt_inds = torch.LongTensor([-1, 1, 2, 0])
-        >>> labels = torch.LongTensor([0, 3, 4, 0])
-        >>> self = AssignResult(num_gts, gt_inds, max_overlaps, labels)
-        >>> print(str(self))  # xdoctest: +IGNORE_WANT
-        <AssignResult(num_gts=9, gt_inds.shape=(4,), max_overlaps.shape=(4,),
-                      labels.shape=(4,))>
-        >>> # Force addition of gt labels (when adding gt as proposals)
-        >>> new_labels = torch.LongTensor([3, 4, 5])
-        >>> self.add_gt_(new_labels)
-        >>> print(str(self))  # xdoctest: +IGNORE_WANT
-        <AssignResult(num_gts=9, gt_inds.shape=(7,), max_overlaps.shape=(7,),
-                      labels.shape=(7,))>
     """
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> cab23055 (Updated)
-    def __init__(self, num_gts: int, gt_inds: Tensor, max_overlaps: Tensor,
-                 labels: Tensor) -> None:
-        self.num_gts = num_gts
-        self.gt_inds = gt_inds
-        self.max_overlaps = max_overlaps
-        self.labels = labels
-        # Interface for possible user-defined properties
-        self._extra_properties = {}
-
-<<<<<<< HEAD
-=======
-=======
     def __init__(self, num_gts: int, gt_inds: Tensor, max_overlaps: Tensor = None,
-             labels: Tensor = None) -> None:
+                 labels: Tensor = None) -> None:
         self.num_gts = num_gts
         self.gt_inds = gt_inds
-        # print(f"[DEBUG AssignResult __init__] max_overlaps={max_overlaps}")
         self.max_overlaps = max_overlaps
         self.labels = labels
         self._extra_properties = {}
 
-
->>>>>>> ab9e3478 (Updated)
->>>>>>> cab23055 (Updated)
     @property
     def num_preds(self):
         """int: the number of predictions in this assignment"""
@@ -114,26 +76,7 @@ class AssignResult(util_mixins.NiceRepr):
 
     @classmethod
     def random(cls, **kwargs):
-        """Create random AssignResult for tests or debugging.
-
-        Args:
-            num_preds: number of predicted boxes
-            num_gts: number of true boxes
-            p_ignore (float): probability of a predicted box assigned to an
-                ignored truth
-            p_assigned (float): probability of a predicted box not being
-                assigned
-            p_use_label (float | bool): with labels or not
-            rng (None | int | numpy.random.RandomState): seed or state
-
-        Returns:
-            :obj:`AssignResult`: Randomly generated assign results.
-
-        Example:
-            >>> from mmdet.models.task_modules.assigners.assign_result import *  # NOQA
-            >>> self = AssignResult.random()
-            >>> print(self.info)
-        """
+        """Create random AssignResult for tests or debugging."""
         from ..samplers.sampling_result import ensure_rng
         rng = ensure_rng(kwargs.get('rng', None))
 
@@ -152,49 +95,33 @@ class AssignResult(util_mixins.NiceRepr):
             max_overlaps = torch.zeros(num_preds, dtype=torch.float32)
             gt_inds = torch.zeros(num_preds, dtype=torch.int64)
             labels = torch.zeros(num_preds, dtype=torch.int64)
-
         else:
             import numpy as np
-
-            # Create an overlap for each predicted box
             max_overlaps = torch.from_numpy(rng.rand(num_preds))
-
-            # Construct gt_inds for each predicted box
             is_assigned = torch.from_numpy(rng.rand(num_preds) < p_assigned)
-            # maximum number of assignments constraints
             n_assigned = min(num_preds, min(num_gts, is_assigned.sum()))
-
             assigned_idxs = np.where(is_assigned)[0]
             rng.shuffle(assigned_idxs)
             assigned_idxs = assigned_idxs[0:n_assigned]
             assigned_idxs.sort()
-
             is_assigned[:] = 0
             is_assigned[assigned_idxs] = True
-
             is_ignore = torch.from_numpy(
                 rng.rand(num_preds) < p_ignore) & is_assigned
-
             gt_inds = torch.zeros(num_preds, dtype=torch.int64)
-
             true_idxs = np.arange(num_gts)
             rng.shuffle(true_idxs)
             true_idxs = torch.from_numpy(true_idxs)
             gt_inds[is_assigned] = true_idxs[:n_assigned].long()
-
             gt_inds = torch.from_numpy(
                 rng.randint(1, num_gts + 1, size=num_preds))
             gt_inds[is_ignore] = -1
             gt_inds[~is_assigned] = 0
             max_overlaps[~is_assigned] = 0
-
             if num_classes == 0:
                 labels = torch.zeros(num_preds, dtype=torch.int64)
             else:
                 labels = torch.from_numpy(
-                    # remind that we set FG labels to [0, num_class-1]
-                    # since mmdet v2.0
-                    # BG cat_id: num_class
                     rng.randint(0, num_classes, size=num_preds))
                 labels[~is_assigned] = 0
 
@@ -202,26 +129,10 @@ class AssignResult(util_mixins.NiceRepr):
         return self
 
     def add_gt_(self, gt_labels):
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-
-        # print("[DEBUG in add_gt_] max_overlaps:", self.max_overlaps)
-        # print("[DEBUG in add_gt_] gt_labels:", gt_labels)
-
->>>>>>> ab9e3478 (Updated)
->>>>>>> cab23055 (Updated)
-        """Add ground truth as assigned results.
-
-        Args:
-            gt_labels (torch.Tensor): Labels of gt boxes
-        """
+        """Add ground truth as assigned results."""
         self_inds = torch.arange(
             1, len(gt_labels) + 1, dtype=torch.long, device=gt_labels.device)
         self.gt_inds = torch.cat([self_inds, self.gt_inds])
-
         self.max_overlaps = torch.cat(
             [self.max_overlaps.new_ones(len(gt_labels)), self.max_overlaps])
-
         self.labels = torch.cat([gt_labels, self.labels])
