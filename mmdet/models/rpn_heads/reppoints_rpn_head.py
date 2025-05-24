64982cbc217c6fef399c20f3ec3c57eafebc1cf3
SSSerpenttt <77667524+SSSerpenttt@users.noreply.github.com>
2025-05-24 22:44:47 +0800

    Update reppoints_rpn_head.py

--- mmdet/models/rpn_heads/reppoints_rpn_head.py
+++ mmdet/models/rpn_heads/reppoints_rpn_head.py
@@ -6,6 +6,7 @@ from mmcv.ops import DeformConv2d
 from mmengine.config import ConfigDict
 from mmengine.structures import InstanceData
 from torch import Tensor
+from typing import Sequence, Tuple, List, Dict 
 
 from mmdet.structures.bbox import HorizontalBoxes, BaseBoxes
 from mmdet.registry import MODELS, TASK_UTILS
