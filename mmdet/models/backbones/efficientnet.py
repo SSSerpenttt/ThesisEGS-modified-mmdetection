4c214ad43a972d3590ddaa633913aff11be05130
SSSerpenttt <77667524+SSSerpenttt@users.noreply.github.com>
2025-05-26 13:48:48 +0800

    Update efficientnet.py

--- mmdet/models/backbones/efficientnet.py
+++ mmdet/models/backbones/efficientnet.py
@@ -10,8 +10,8 @@ from mmcv.cnn.bricks import ConvModule, DropPath
 from mmengine.model import BaseModule, Sequential
 
 from mmdet.registry import MODELS
-from mmdet.models.layers import InvertedResidual, SELayer
-from mmdet.models.utils import make_divisible
+from ..layers import InvertedResidual, SELayer
+from ..utils import make_divisible
 
 
 class EdgeResidual(BaseModule):
@@ -88,8 +88,6 @@ class EdgeResidual(BaseModule):
             conv_cfg=conv_cfg,
             norm_cfg=norm_cfg,
             act_cfg=None)
-
-        self.out_channels = out_channels
 
     def forward(self, x):
 
@@ -196,29 +194,49 @@ class EfficientNet(BaseModule):
     layer_settings = {
         'b': [[[3, 32, 0, 2, 0, -1]],
               [[3, 16, 4, 1, 1, 0]],
-              [[3, 24, 4, 2, 6, 0], [3, 24, 4, 1, 6, 0]],
-              [[5, 40, 4, 2, 6, 0], [5, 40, 4, 1, 6, 0]],
-              [[3, 80, 4, 2, 6, 0], [3, 80, 4, 1, 6, 0], 
-               [3, 80, 4, 1, 6, 0], [5, 112, 4, 1, 6, 0],
-               [5, 112, 4, 1, 6, 0], [5, 112, 4, 1, 6, 0]],
-              [[5, 192, 4, 2, 6, 0], [5, 192, 4, 1, 6, 0],
-               [5, 192, 4, 1, 6, 0], [5, 192, 4, 1, 6, 0],
+              [[3, 24, 4, 2, 6, 0],
+               [3, 24, 4, 1, 6, 0]],
+              [[5, 40, 4, 2, 6, 0],
+               [5, 40, 4, 1, 6, 0]],
+              [[3, 80, 4, 2, 6, 0],
+               [3, 80, 4, 1, 6, 0],
+               [3, 80, 4, 1, 6, 0],
+               [5, 112, 4, 1, 6, 0],
+               [5, 112, 4, 1, 6, 0],
+               [5, 112, 4, 1, 6, 0]],
+              [[5, 192, 4, 2, 6, 0],
+               [5, 192, 4, 1, 6, 0],
+               [5, 192, 4, 1, 6, 0],
+               [5, 192, 4, 1, 6, 0],
                [3, 320, 4, 1, 6, 0]],
-              [[1, 1280, 0, 1, 0, -1]]],
+              [[1, 1280, 0, 1, 0, -1]]
+              ],
         'e': [[[3, 32, 0, 2, 0, -1]],
               [[3, 24, 0, 1, 3, 1]],
-              [[3, 32, 0, 2, 8, 1], [3, 32, 0, 1, 8, 1]],
-              [[3, 48, 0, 2, 8, 1], [3, 48, 0, 1, 8, 1],
-               [3, 48, 0, 1, 8, 1], [3, 48, 0, 1, 8, 1]],
-              [[5, 96, 0, 2, 8, 0], [5, 96, 0, 1, 8, 0],
-               [5, 96, 0, 1, 8, 0], [5, 96, 0, 1, 8, 0],
-               [5, 96, 0, 1, 8, 0], [5, 144, 0, 1, 8, 0],
-               [5, 144, 0, 1, 8, 0], [5, 144, 0, 1, 8, 0],
+              [[3, 32, 0, 2, 8, 1],
+               [3, 32, 0, 1, 8, 1]],
+              [[3, 48, 0, 2, 8, 1],
+               [3, 48, 0, 1, 8, 1],
+               [3, 48, 0, 1, 8, 1],
+               [3, 48, 0, 1, 8, 1]],
+              [[5, 96, 0, 2, 8, 0],
+               [5, 96, 0, 1, 8, 0],
+               [5, 96, 0, 1, 8, 0],
+               [5, 96, 0, 1, 8, 0],
+               [5, 96, 0, 1, 8, 0],
+               [5, 144, 0, 1, 8, 0],
+               [5, 144, 0, 1, 8, 0],
+               [5, 144, 0, 1, 8, 0],
                [5, 144, 0, 1, 8, 0]],
-              [[5, 192, 0, 2, 8, 0], [5, 192, 0, 1, 8, 0]],
-              [[1, 1280, 0, 1, 0, -1]]]
-    }
+              [[5, 192, 0, 2, 8, 0],
+               [5, 192, 0, 1, 8, 0]],
+              [[1, 1280, 0, 1, 0, -1]]
+              ]
+    }  # yapf: disable
 
+    # Parameters to build different kinds of architecture.
+    # From left to right: scaling factor for width, scaling factor for depth,
+    # resolution.
     arch_settings = {
         'b0': (1.0, 1.0, 224),
         'b1': (1.0, 1.1, 240),
@@ -235,9 +253,9 @@ class EfficientNet(BaseModule):
     }
 
     def __init__(self,
-                 arch='b3',
+                 arch='b0',
                  drop_path_rate=0.,
-                 out_indices=(3, 4, 5),  # Default for BiFPN integration
+                 out_indices=(6, ),
                  frozen_stages=0,
                  conv_cfg=dict(type='Conv2dAdaptivePadding'),
                  norm_cfg=dict(type='BN', eps=1e-3),
@@ -246,18 +264,27 @@ class EfficientNet(BaseModule):
                  with_cp=False,
                  init_cfg=[
                      dict(type='Kaiming', layer='Conv2d'),
-                     dict(type='Constant', layer=['_BatchNorm', 'GroupNorm'], val=1)
+                     dict(
+                         type='Constant',
+                         layer=['_BatchNorm', 'GroupNorm'],
+                         val=1)
                  ]):
-        super().__init__(init_cfg)
-        
-        # Validation checks
+        super(EfficientNet, self).__init__(init_cfg)
         assert arch in self.arch_settings, \
-            f'Invalid arch: {arch}. Choices: {list(self.arch_settings.keys())}'
-        assert all(i in range(len(self.layer_settings[arch[0]])) for i in out_indices), \
-            f'Invalid out_indices {out_indices} for arch {arch}'
-            
+            f'"{arch}" is not one of the arch_settings ' \
+            f'({", ".join(self.arch_settings.keys())})'
         self.arch_setting = self.arch_settings[arch]
-        self.layer_setting = self.layer_settings[arch[0]]
+        self.layer_setting = self.layer_settings[arch[:1]]
+        for index in out_indices:
+            if index not in range(0, len(self.layer_setting)):
+                raise ValueError('the item in out_indices must in '
+                                 f'range(0, {len(self.layer_setting)}). '
+                                 f'But received {index}')
+
+        if frozen_stages not in range(len(self.layer_setting) + 1):
+            raise ValueError('frozen_stages must be in range(0, '
+                             f'{len(self.layer_setting) + 1}). '
+                             f'But received {frozen_stages}')
         self.drop_path_rate = drop_path_rate
         self.out_indices = out_indices
         self.frozen_stages = frozen_stages
@@ -266,18 +293,15 @@ class EfficientNet(BaseModule):
         self.act_cfg = act_cfg
         self.norm_eval = norm_eval
         self.with_cp = with_cp
-        
-        # Build model
-        self._build_model()
 
-    def _build_model(self):
-        """Construct the model architecture."""
-        self.layer_setting = model_scaling(self.layer_setting, self.arch_setting)
-        
-        # Initial convolution
+        self.layer_setting = model_scaling(self.layer_setting,
+                                           self.arch_setting)
         block_cfg_0 = self.layer_setting[0][0]
+        block_cfg_last = self.layer_setting[-1][0]
         self.in_channels = make_divisible(block_cfg_0[1], 8)
-        self.layers = nn.ModuleList([
+        self.out_channels = block_cfg_last[1]
+        self.layers = nn.ModuleList()
+        self.layers.append(
             ConvModule(
                 in_channels=3,
                 out_channels=self.in_channels,
@@ -286,19 +310,14 @@ class EfficientNet(BaseModule):
                 padding=block_cfg_0[0] // 2,
                 conv_cfg=self.conv_cfg,
                 norm_cfg=self.norm_cfg,
-                act_cfg=self.act_cfg)
-        ])
-        
-        # Intermediate layers
-        self._build_intermediate_layers()
-        
-        # Final convolution if needed
+                act_cfg=self.act_cfg))
+        self.make_layer()
+        # Avoid building unused layers in mmdetection.
         if len(self.layers) < max(self.out_indices) + 1:
-            block_cfg_last = self.layer_setting[-1][0]
             self.layers.append(
                 ConvModule(
                     in_channels=self.in_channels,
-                    out_channels=block_cfg_last[1],
+                    out_channels=self.out_channels,
                     kernel_size=block_cfg_last[0],
                     stride=block_cfg_last[3],
                     padding=block_cfg_last[0] // 2,
@@ -306,98 +325,84 @@ class EfficientNet(BaseModule):
                     norm_cfg=self.norm_cfg,
                     act_cfg=self.act_cfg))
 
-    def _build_intermediate_layers(self):
-        """Construct the intermediate layers with drop path."""
+    def make_layer(self):
+        # Without the first and the final conv block.
         layer_setting = self.layer_setting[1:-1]
-        total_blocks = sum(len(x) for x in layer_setting)
-        dpr = torch.linspace(0, self.drop_path_rate, total_blocks).tolist()
-        
+
+        total_num_blocks = sum([len(x) for x in layer_setting])
         block_idx = 0
+        dpr = [
+            x.item()
+            for x in torch.linspace(0, self.drop_path_rate, total_num_blocks)
+        ]  # stochastic depth decay rule
+
         for i, layer_cfg in enumerate(layer_setting):
+            # Avoid building unused layers in mmdetection.
             if i > max(self.out_indices) - 1:
                 break
-                
             layer = []
-            for block_cfg in layer_cfg:
-                layer.append(self._build_block(block_cfg, dpr[block_idx]))
-                block_idx += 1
-                
-            self.layers.append(Sequential(*layer))
-            self.in_channels = layer[-1].out_channels
+            for i, block_cfg in enumerate(layer_cfg):
+                (kernel_size, out_channels, se_ratio, stride, expand_ratio,
+                 block_type) = block_cfg
 
-    def _build_block(self, block_cfg, drop_path_rate):
-      """Build a single block with strict channel validation."""
-      kernel_size, out_channels, se_ratio, stride, expand_ratio, block_type = block_cfg
-      
-      # Ensure all channels are divisible by 8
-      out_channels = make_divisible(out_channels, 8)
-      mid_channels = make_divisible(self.in_channels * expand_ratio, 8)
-      
-      # For EfficientNet-b3, we need to ensure specific channel counts
-      if self.arch_setting == self.arch_settings['b3']:
-          if expand_ratio == 6:  # Standard expansion for b3
-              mid_channels = make_divisible(self.in_channels * 6, 8)
-      
-      # Configure SE layer
-      se_cfg = None
-      if se_ratio > 0:
-          se_cfg = dict(
-              channels=mid_channels,
-              ratio=se_ratio,
-              act_cfg=(self.act_cfg, dict(type='Sigmoid')))
-      
-      # Select block type
-      if block_type == 1:  # EdgeResidual
-          with_residual = not (expand_ratio == 3 and stride > 1)
-          if not with_residual:
-              expand_ratio = 4  # Adjust for non-residual path
-          mid_channels = make_divisible(self.in_channels * expand_ratio, 8)
-          
-          return EdgeResidual(
-              in_channels=self.in_channels,
-              out_channels=out_channels,
-              mid_channels=mid_channels,
-              kernel_size=kernel_size,
-              stride=stride,
-              se_cfg=se_cfg,
-              with_residual=with_residual,
-              conv_cfg=self.conv_cfg,
-              norm_cfg=self.norm_cfg,
-              act_cfg=self.act_cfg,
-              drop_path_rate=drop_path_rate,
-              with_cp=self.with_cp)
-      else:  # InvertedResidual
-          return InvertedResidual(
-              in_channels=self.in_channels,
-              out_channels=out_channels,
-              mid_channels=mid_channels,
-              kernel_size=kernel_size,
-              stride=stride,
-              se_cfg=se_cfg,
-              conv_cfg=self.conv_cfg,
-              norm_cfg=self.norm_cfg,
-              act_cfg=self.act_cfg,
-              drop_path_rate=drop_path_rate,
-              with_cp=self.with_cp,
-              with_expand_conv=(mid_channels != self.in_channels))
+                mid_channels = int(self.in_channels * expand_ratio)
+                out_channels = make_divisible(out_channels, 8)
+                if se_ratio <= 0:
+                    se_cfg = None
+                else:
+                    # In mmdetection, the `divisor` is deleted to align
+                    # the logic of SELayer with mmpretrain.
+                    se_cfg = dict(
+                        channels=mid_channels,
+                        ratio=expand_ratio * se_ratio,
+                        act_cfg=(self.act_cfg, dict(type='Sigmoid')))
+                if block_type == 1:  # edge tpu
+                    if i > 0 and expand_ratio == 3:
+                        with_residual = False
+                        expand_ratio = 4
+                    else:
+                        with_residual = True
+                    mid_channels = int(self.in_channels * expand_ratio)
+                    if se_cfg is not None:
+                        # In mmdetection, the `divisor` is deleted to align
+                        # the logic of SELayer with mmpretrain.
+                        se_cfg = dict(
+                            channels=mid_channels,
+                            ratio=se_ratio * expand_ratio,
+                            act_cfg=(self.act_cfg, dict(type='Sigmoid')))
+                    block = partial(EdgeResidual, with_residual=with_residual)
+                else:
+                    block = InvertedResidual
+                layer.append(
+                    block(
+                        in_channels=self.in_channels,
+                        out_channels=out_channels,
+                        mid_channels=mid_channels,
+                        kernel_size=kernel_size,
+                        stride=stride,
+                        se_cfg=se_cfg,
+                        conv_cfg=self.conv_cfg,
+                        norm_cfg=self.norm_cfg,
+                        act_cfg=self.act_cfg,
+                        drop_path_rate=dpr[block_idx],
+                        with_cp=self.with_cp,
+                        # In mmdetection, `with_expand_conv` is set to align
+                        # the logic of InvertedResidual with mmpretrain.
+                        with_expand_conv=(mid_channels != self.in_channels)))
+                self.in_channels = out_channels
+                block_idx += 1
+            self.layers.append(Sequential(*layer))
 
     def forward(self, x):
-        """Enhanced forward pass with better input handling."""
-        if isinstance(x, (list, tuple)):
-            x = x[0]  # Handle multi-input cases
-            
         outs = []
         for i, layer in enumerate(self.layers):
             x = layer(x)
             if i in self.out_indices:
                 outs.append(x)
-                if len(outs) == len(self.out_indices):
-                    break  # Early exit if we have all required features
-                    
+
         return tuple(outs)
 
     def _freeze_stages(self):
-        """Freeze stages for transfer learning."""
         for i in range(self.frozen_stages):
             m = self.layers[i]
             m.eval()
@@ -405,8 +410,7 @@ class EfficientNet(BaseModule):
                 param.requires_grad = False
 
     def train(self, mode=True):
-        """Enhanced train mode with norm freezing option."""
-        super().train(mode)
+        super(EfficientNet, self).train(mode)
         self._freeze_stages()
         if mode and self.norm_eval:
             for m in self.modules():
