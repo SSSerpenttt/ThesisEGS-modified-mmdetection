2ea07cdef7d88c02b7b41d9abf185cc6aea84faf
SSSerpenttt <77667524+SSSerpenttt@users.noreply.github.com>
2025-06-03 19:37:35 +0800

    Update modified-maskrcnn.config.py

--- mmdet/configs/_custom_/modified-maskrcnn.config.py
+++ mmdet/configs/_custom_/modified-maskrcnn.config.py
@@ -1,3 +1,5 @@
+default_scope = 'mmdet'
+
 # Enhanced training configuration
 train_cfg = dict(
     type='EpochBasedTrainLoop',
@@ -8,204 +10,181 @@ train_cfg = dict(
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 
-# Optimized model training configuration
-model_train_cfg = dict(
-    rpn=dict(
-        assigner=dict(
-            type='mmdet.MaxIoUAssigner',
-            pos_iou_thr=0.45,
-            neg_iou_thr=0.5,
-            min_pos_iou=0.2,
-            match_low_quality=True,
-            ignore_iof_thr=-1
-        ),
-        sampler=dict(
-            type='mmdet.RandomSampler',
-            num=256,
-            pos_fraction=0.5,
-            neg_pos_ub=-1,
-            add_gt_as_proposals=True
-        ),
-        allowed_border=-1,
-        pos_weight=-1,
-        debug=False
-    ),
-    rcnn=dict(
-        assigner=dict(
-            type='mmdet.MaxIoUAssigner',
-            pos_iou_thr=0.45,
-            neg_iou_thr=0.5,  # Adjusted from 0.3 for better precision
-            min_pos_iou=0.2,
-            match_low_quality=False,  # Stricter matching for RCNN
-            ignore_iof_thr=-1
-        ),
-        sampler=dict(
-            type='mmdet.RandomSampler',
-            num=512,
-            pos_fraction=0.35,  # Increased positive samples
-            neg_pos_ub=-1,
-            add_gt_as_proposals=True
-        ),
-        pos_weight=-1,
-        debug=False,
-        mask_size=56
-    )
-)
 
-# Enhanced test configuration
-model_test_cfg = dict(
-    rpn=dict(
-        score_thr=0.015,
-        nms_pre=3000,  # Increased from 300
-        max_per_img=1000,
-        nms=dict(type='nms', iou_threshold=0.5),
-        min_bbox_size=0
-    ),
-    rcnn=dict(
-        score_thr=0.015,
-        nms=dict(type='soft_nms', iou_threshold=0.5),  # Changed to soft_nms
-        max_per_img=200,  # Increased from 100
-        mask_thr_binary=0.45  # Adjusted threshold
-    )
-)
-
-# Model architecture
+# Model architecture using FCOS as anchor-free RPN
 model = dict(
-    type='mmdet.MaskRCNN',
+    type='MaskRCNN',
     backbone=dict(
-        type='mmdet.EfficientNet',
+        type='EfficientNet',
         arch='b3',
-        out_indices=(2, 3, 4, 5),  # Correct stages for channel dimensions
-        norm_cfg=dict(type='mmdet.BN', requires_grad=True, momentum=0.1, eps=1e-3),
+        out_indices=(2, 3, 4, 5),
+        norm_cfg=dict(type='BN', requires_grad=True, momentum=0.1, eps=1e-3),
         norm_eval=False,
-        frozen_stages=1
+        frozen_stages=0
     ),
     neck=dict(
-        type='mmdet.BiFPN',
-        in_channels=[32, 48, 136, 384],  # Must match EfficientNet-b3 stages
-        out_channels=256,  # This will feed into RPN head
+        type='BiFPN',
+        in_channels=[32, 48, 136, 384],  # EfficientNet-B3 output dims
+        out_channels=256,
         num_outs=4,
-        stack=3,
+        stack=2,
         start_level=0,
         end_level=-1,
-        norm_cfg=dict(type='mmdet.BN', requires_grad=True),
-        add_extra_convs='None'
+        norm_cfg=dict(type='BN', requires_grad=True),
+        add_extra_convs='none'
     ),
     rpn_head=dict(
-        type='mmdet.RepPointsRPNHead',
+        type='RepPointsHead',
         in_channels=256,
         feat_channels=256,
         point_feat_channels=256,
+        stacked_convs=3,
         num_points=9,
-        gradient_mul=0.1,
-        point_strides=[4, 8, 16, 32],
-        point_base_scale=2,
+        gradient_mul=0.3,
+        point_strides=[8, 16, 32, 64],
+        point_base_scale=4,
+        norm_cfg=dict(type='GN', num_groups=32, requires_grad=True),
         loss_cls=dict(
-            type='mmdet.CrossEntropyLoss',
+            type='FocalLoss',
             use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
             loss_weight=1.0),
         loss_bbox_init=dict(
-            type='mmdet.SmoothL1Loss', beta=1.0/9.0, loss_weight=0.05),
+            type='SmoothL1Loss',
+            beta=1.0 / 9.0,
+            loss_weight=0.5),
         loss_bbox_refine=dict(
-            type='mmdet.SmoothL1Loss', beta=1.0/9.0, loss_weight=0.05),
-        transform_method='minmax',
-        topk=1000,
-        num_classes=1,  # Must be 1 for RPN
-        train_cfg=dict(
-            init=dict(
-                assigner=dict(type='mmdet.PointAssigner', scale=32, pos_num=5),
-                allowed_border=-1,
-                pos_weight=-1,
-                debug=False),
-            refine=dict(
-                assigner=dict(
-                    type='mmdet.MaxIoUAssigner',
-                    pos_iou_thr=0.45,
-                    neg_iou_thr=0.5,
-                    min_pos_iou=2,
-                    ignore_iof_thr=-1),
-                sampler=dict(
-                    type='mmdet.RandomSampler',
-                    num=512,
-                    pos_fraction=0.5,
-                    neg_pos_ub=-1,
-                    add_gt_as_proposals=False),
-                allowed_border=0,
-                pos_weight=-1,
-                debug=False)),
-        test_cfg=dict(
-            nms_pre=2000,
-            min_bbox_size=0,
-            score_thr=0.05,
-            nms=dict(type='mmdet.nms', iou_threshold=0.7),
-            max_per_img=1000)
+            type='SmoothL1Loss',
+            beta=1.0 / 9.0,
+            loss_weight=1.0)
     ),
     roi_head=dict(
-        type='mmdet.StandardRoIHead',
+        type='StandardRoIHead',
         bbox_roi_extractor=dict(
-            type='mmdet.SingleRoIExtractor',
+            type='SingleRoIExtractor',
             roi_layer=dict(
-                type='RoIAlign',
-                output_size=7,
-                sampling_ratio=2
-            ),
-            out_channels=256,  # Must match neck output channels
-            featmap_strides=[4, 8, 16, 32],
+                type='RoIAlign', output_size=7, sampling_ratio=2),
+            out_channels=256,
+            featmap_strides=[8, 16, 32, 64],
             finest_scale=56
         ),
         bbox_head=dict(
-            type='mmdet.Shared2FCBBoxHead',
-            in_channels=256,  # Must match roi extractor out_channels
+            type='Shared2FCBBoxHead',
+            in_channels=256,
             fc_out_channels=1024,
             roi_feat_size=7,
-            num_classes=19,
+            num_classes=17,
             reg_decoded_bbox=True,
             loss_cls=dict(
-                type='mmdet.FocalLoss',
+                type='FocalLoss',
                 use_sigmoid=True,
                 gamma=2.0,
                 alpha=0.25,
-                loss_weight=1.0
+                loss_weight=2.5
             ),
             loss_bbox=dict(
-                type='mmdet.IoULoss',
-                loss_weight=1.0
+                type='IoULoss',
+                loss_weight=2.5
             )
         ),
         mask_roi_extractor=dict(
-            type='mmdet.SingleRoIExtractor',
+            type='SingleRoIExtractor',
             roi_layer=dict(
-                type='RoIAlign',
-                output_size=28,
-                sampling_ratio=2
-            ),
-            out_channels=256,  # Must match neck output channels
-            featmap_strides=[4, 8, 16, 32, 64],
+                type='RoIAlign', output_size=28, sampling_ratio=2),
+            out_channels=256,
+            featmap_strides=[8, 16, 32, 64],
             finest_scale=56
         ),
         mask_head=dict(
-            type='mmdet.FCNMaskHead',
+            type='FCNMaskHead',
             num_convs=4,
-            in_channels=256,  # Must match roi extractor out_channels
+            in_channels=256,
             conv_out_channels=256,
-            num_classes=19,
+            num_classes=17,
             loss_mask=dict(
-                type='mmdet.DiceLoss',
-                loss_weight=1.0,
-                activate=False,
+                type='DiceLoss',
+                loss_weight=0.15,
+                activate=True,
                 eps=1e-5
             )
         )
     ),
-    train_cfg=model_train_cfg,
-    test_cfg=model_test_cfg
+    # Ensure these are defined elsewhere or added directly here
+    train_cfg=dict(
+        rpn=dict(
+            init=dict(
+                assigner=dict(
+                    type='PointAssigner',
+                    scale=4,
+                    pos_num=1
+                ),
+                allowed_border=-1,
+                pos_weight=-1,
+                debug=False
+            ),
+            refine=dict(
+                assigner=dict(
+                    type='MaxIoUAssigner',
+                    pos_iou_thr=0.5,
+                    neg_iou_thr=0.4,
+                    min_pos_iou=0,
+                    match_low_quality=True,
+                    ignore_iof_thr=-1
+                ),
+                sampler=dict(
+                    type='RandomSampler',
+                    num=256,
+                    pos_fraction=0.5,
+                    neg_pos_ub=-1,
+                    add_gt_as_proposals=False
+                ),
+                allowed_border=-1,
+                pos_weight=-1,
+                debug=False
+            )
+        ),
+        rcnn=dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.5,
+                neg_iou_thr=0.5,
+                min_pos_iou=0.5,
+                match_low_quality=False
+            ),
+            sampler=dict(
+                type='RandomSampler',
+                num=512,
+                pos_fraction=0.25,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=True
+            ),
+            mask_size=28,
+            pos_weight=-1,
+            debug=False
+        )
+    ),
+    test_cfg=dict(
+        rpn=dict(
+            nms_pre=1000,
+            max_per_img=1000,
+            nms=dict(type='nms', iou_threshold=0.7),
+            min_bbox_size=0
+        ),
+        rcnn=dict(
+            score_thr=0.05,
+            nms=dict(type='nms', iou_threshold=0.5),
+            max_per_img=100,
+            mask_thr_binary=0.5
+        )
+    )
 )
 
+
 # Dataset configuration
-data_root = '/content/GroupEGS-Thesis-Dataset/'
+data_root = '/content/group_egs_augmented/'
 classes = (
     'damage-crack', 'damage-dent', 'damage-scratch', 'damage-tear',
-    'depth-deep', 'depth-shallow',  # <-- add these two here
     'panel-apillar', 'panel-bonnet', 'panel-bpillar',
     'panel-cpillar', 'panel-dpillar', 'panel-fender',
     'panel-frontdoor', 'panel-qpanel', 'panel-reardoor',
@@ -215,11 +194,23 @@ classes = (
 metainfo = {
     'classes': classes,
     'palette': [
-        (220, 20, 60), (119, 11, 32), (0, 128, 0), (0, 0, 255),
-        (255, 140, 0), (255, 215, 0), (128, 0, 128), (255, 105, 180),
-        (64, 224, 208), (123, 104, 238), (255, 69, 0), (154, 205, 50),
-        (70, 130, 180), (95, 158, 160), (255, 20, 147), (0, 191, 255),
-        (189, 183, 107), (0, 255, 127), (160, 82, 45)
+        (220, 20, 60),    # Red
+        (0, 128, 0),      # Green
+        (0, 0, 255),      # Blue
+        (255, 140, 0),    # Orange
+        (255, 215, 0),    # Gold
+        (128, 0, 128),    # Purple
+        (255, 105, 180),  # Pink
+        (64, 224, 208),   # Turquoise
+        (123, 104, 238),  # Slate Blue
+        (255, 69, 0),     # Orange Red
+        (154, 205, 50),   # Yellow Green
+        (70, 130, 180),   # Steel Blue
+        (95, 158, 160),   # Cadet Blue
+        (255, 20, 147),   # Deep Pink
+        (0, 191, 255),    # Sky Blue
+        (189, 183, 107),  # Khaki
+        (160, 82, 45),    # Sienna
     ]
 }
 
@@ -230,78 +221,70 @@ img_norm_cfg = dict(
 )
 
 
-train_pipeline = [
-    dict(type='mmdet.LoadImageFromFile'),
-    dict(type='mmdet.LoadAnnotations', with_bbox=True, with_label=True, with_mask=True),
-    dict(type='mmdet.Resize', scale=(896, 896), keep_ratio=True),
-    dict(type='mmdet.EfficientNetPreprocessor',
-         size_divisor=32,
-         mean=img_norm_cfg['mean'],
-         std=img_norm_cfg['std'],
-         to_rgb=img_norm_cfg['to_rgb']),
-    dict(type='RenameGtLabels'),
-    dict(
-        type='mmdet.TensorPackDetInputs',
-        meta_keys=('img_id', 'img_path', 'img_shape', 'ori_shape', 'img_shape', 'scale_factor'),
-    )
-]
-
 train_dataloader = dict(
-    batch_size=1,  # Will stack into (B, C, H, W)
-    num_workers=0,
-    persistent_workers=False,
-    sampler=dict(type='mmdet.DefaultSampler', shuffle=True),
+    batch_size=2,  # Will stack into (B, C, H, W)
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    collate_fn=dict(type='default_collate'),
     dataset=dict(
-        type='mmdet.CocoDataset',
+        type='CocoDataset',
         data_root=data_root,
-        ann_file='/content/GroupEGS-Thesis-Dataset/train/_annotations_cleaned_final.coco.json',
-        data_prefix=dict(img='/content/GroupEGS-Thesis-Dataset/train/'),
+        ann_file='/content/group_egs_augmented/train/_annotations.coco.json',
+        data_prefix=dict(img='/content/group_egs_augmented/train/'),
         metainfo=metainfo,
         filter_cfg=dict(filter_empty_gt=True, min_size=32),
-        pipeline=train_pipeline
-    ),
+        pipeline = [
+            dict(type='LoadImageFromFile'),
+            dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+            dict(type='Resize', scale=(1333, 800), keep_ratio=True),
+            dict(type='Normalize', mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True),
+            dict(type='Pad', size_divisor=32),
+            dict(
+                type='PackDetInputs',
+                meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape', 'scale_factor', 'pad_shape')  # ✅ include 'pad_shape'
+            )
+        ]
+    )
 )
 
-
 val_dataloader = dict(
-    batch_size=1,
-    num_workers=0,
-    persistent_workers=False,
+    batch_size=2,
+    num_workers=2,
+    persistent_workers=True,
     pin_memory=True,
-    sampler=dict(type='mmdet.DefaultSampler', shuffle=False),
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    collate_fn=dict(type='default_collate'),
     dataset=dict(
-        type='mmdet.CocoDataset',
+        type='CocoDataset',
         data_root=data_root,
-        ann_file='/content/GroupEGS-Thesis-Dataset/valid/_annotations_cleaned_final.coco.json',
-        data_prefix=dict(img='/content/GroupEGS-Thesis-Dataset/valid/'),
+        ann_file='/content/group_egs_augmented/valid/_annotations.coco.json',
+        data_prefix=dict(img='/content/group_egs_augmented/valid/'),
         metainfo=metainfo,
         filter_cfg=dict(filter_empty_gt=True),
-        pipeline=[
-            dict(type='mmdet.LoadImageFromFile'),
-            dict(type='mmdet.LoadAnnotations', with_bbox=True, with_label=True, with_mask=True),
-            dict(type='mmdet.Resize', scale=(896, 896), keep_ratio=True),
-            dict(type='mmdet.EfficientNetPreprocessor',
-                 size_divisor=32,
-                 mean=img_norm_cfg['mean'],
-                 std=img_norm_cfg['std'],
-                 to_rgb=img_norm_cfg['to_rgb']),
-            dict(type='RenameGtLabels'),
+        pipeline = [
+            dict(type='LoadImageFromFile'),
+            dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+            dict(type='Resize', scale=(1333, 800), keep_ratio=True),
+            dict(type='Normalize', mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True),
+            dict(type='Pad', size_divisor=32),
             dict(
-                type='mmdet.TensorPackDetInputs',
-                meta_keys=('img_id', 'img_path', 'img_shape', 'ori_shape', 'img_shape', 'scale_factor'),
+                type='PackDetInputs',
+                meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape', 'scale_factor', 'pad_shape')  # ✅ include 'pad_shape'
             )
         ]
     )
 )
 
 test_dataloader = val_dataloader.copy()
-test_dataloader['dataset']['ann_file'] = '/content/GroupEGS-Thesis-Dataset/test/_annotations_cleaned_final.coco.json'
-test_dataloader['dataset']['data_prefix'] = dict(img='/content/GroupEGS-Thesis-Dataset/test/')
+test_dataloader['dataset']['ann_file'] = '/content/group_egs_augmented/test/_annotations.coco.json'
+test_dataloader['dataset']['data_prefix'] = dict(img='/content/group_egs_augmented/test/')
+test_dataloader['collate_fn'] = dict(type='default_collate')  # ✅ Add this too
 
 # Enhanced evaluation
 val_evaluator = dict(
-    type='mmdet.CocoMetric',
-    ann_file='/content/GroupEGS-Thesis-Dataset/valid/_annotations_cleaned_final.coco.json',
+    type='CocoMetric',
+    ann_file='/content/group_egs_augmented/valid/_annotations.coco.json',
     metric=['bbox', 'segm'],
     classwise=True,
     # iou_thrs=[0.5, 0.75],  # Added stricter IoU threshold
@@ -309,12 +292,12 @@ val_evaluator = dict(
 )
 
 test_evaluator = val_evaluator.copy()
-test_evaluator['ann_file'] = '/content/GroupEGS-Thesis-Dataset/test/_annotations_cleaned_final.coco.json'
+test_evaluator['ann_file'] = '/content/group_egs_augmented/test/_annotations.coco.json'
 
 # Optimized training configuration
 optim_wrapper = dict(
     optimizer=dict(
-        type='mmdet.AdamW',
+        type='AdamW',
         lr=1e-4,
         weight_decay=0.05,
         betas=(0.9, 0.999)),
@@ -350,27 +333,27 @@ param_scheduler = [
 
 # Enhanced runtime configuration
 default_hooks = dict(
-    timer=dict(type='mmdet.IterTimerHook'),
+    timer=dict(type='IterTimerHook'),
     logger=dict(
-        type='mmdet.LoggerHook',
-        interval=25,
+        type='LoggerHook',
+        interval=100,
         log_metric_by_epoch=True
     ),
-    param_scheduler=dict(type='mmdet.ParamSchedulerHook'),
+    param_scheduler=dict(type='ParamSchedulerHook'),
     checkpoint=dict(
-        type='mmdet.CheckpointHook',
+        type='CheckpointHook',
         interval=50,
         max_keep_ckpts=3,  # Limit checkpoint storage
         save_best='auto',
     ),
-    sampler_seed=dict(type='mmdet.DistSamplerSeedHook'),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
     visualization=dict(
-        type='mmdet.DetVisualizationHook',
+        type='DetVisualizationHook',
         interval=25,
         draw=True
     ),
     early_stop=dict(
-        type='mmdet.EarlyStoppingHook',
+        type='EarlyStoppingHook',
         monitor='coco/bbox_mAP',
         patience=5,
         min_delta=0.005
@@ -394,14 +377,10 @@ fp16 = dict(loss_scale=dict(init_scale=512), opt_level='O1')
 
 # Visualization
 visualizer = dict(
-    type='mmdet.DetLocalVisualizer',
+    type='DetLocalVisualizer',
     vis_backends=[
         dict(type='LocalVisBackend'),
         dict(type='TensorboardVisBackend')  # Added TensorBoard
     ],
     name='visualizer'
 )
-
-#Comment these out if not using weights===========================================================================================================
-# resume = True
-# load_from = '/content/epoch_1.05302025-14-55.pth'
